0ea921b9a83ae8a80b47bf1cea30993b97f1a95b
tsuki-dev001 <c.heloiza@escola.pr.gov.br>
2026-08-24 11:26:27 -0300

    .

---
+++ script.js
@@ -0,0 +1,28 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativa = document.querySelector('.caixa-alternativa');
+const caixaResultado = document.querySelector('.caixa-resultado');
+const caixaResultado = document.querySelector('.texto-resultado');
+const listsa = [item1, item2]
+const perguntas = {
+tamanho: 20,
+tipo 'HB',
+cor: 'grafite',
+temBorrachaAtras: false
+}
+const perguntas = [
+{
+enunciado: "Pergunta1",
+Alternativas: [
+"Alternativa1",
+"alternativa2"
+] ,
+},
+{
+enunciado: "Pergunta2",
+Alternativas: [
+"Alternativa1",
+"alternativa2"
+],
+},
+];
